a8e4e4169c4e9b795b7afa1fa06fe639f98e9bbb
Aanchal6 <aanchalacharya11@gmail.com>
2025-04-18 13:27:27 -0600

    package.json

--- Movies.js
+++ Movies.js
@@ -77,6 +77,6 @@ app.delete('/movies/:title', async (req, res) => {
   }
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// // Start the server
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
