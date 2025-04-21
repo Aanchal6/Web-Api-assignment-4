650b055cf4d69461e9d26a745b0e11a16730fa6a
Aanchal6 <aanchalacharya11@gmail.com>
2025-04-21 12:33:29 -0600

    package.json

--- Movies.js
+++ Movies.js
@@ -6,15 +6,15 @@ require('dotenv').config();
 const app = express();
 app.use(express.json()); 
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DB);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1); // Exit the process if the connection fails (optional)
-  }
-};
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.DB);
+//     console.log("Connected to MongoDB");
+//   } catch (error) {
+//     console.error("MongoDB connection error:", error);
+//     process.exit(1); // Exit the process if the connection fails (optional)
+//   }
+// };
 
 connectDB();
 
--- server.js
+++ server.js
@@ -22,6 +22,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1); // Exit the process if the connection fails (optional)
+  }
+};
+
+connectDB();
 
 var router = express.Router();
 
